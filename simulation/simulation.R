8b13432010534029e6aa5f343f7dd451a7a1e780
XiaodanLyu <wuhandaxue2013@gmail.com>
2020-04-05 20:20:56 -0500

    restart estimation

--- simulation/simulation.R
+++ simulation/simulation.R
@@ -10,13 +10,16 @@ source("simulation/utility-functions.R")
 true.para <- list(beta = c(-13, 2), sig2lu = 0.22, sig2le = 1.23,
                   alpha = c(-20, 5), sig2lb = 0.52)
 
-set.seed(2020)
-system.time(
-  sim_mc(nsim = 3, true.para, rho = 0.9, link = "logit",
-         alts = TRUE, bootstrap = TRUE)
-)
+# set.seed(2020)
+# system.time(
+#   sim_mc(nsim = 3, true.para, rho = 0.9, link = "logit",
+#          alts = TRUE, bootstrap = TRUE)
+# )
 
 library(parallel)
-mclapply(seq(-0.9, 0.6, by = 0.3), sim_mc,
-         nsim = 1000, true.para = true.para, link = "logit",
-         alts = FALSE, bootstrap = FALSE, mc.cores = 6)
+set.seed(200405)
+system.time(
+  mclapply(c(-0.9, -0.6, -0.3, 0, 0.3, 0.6), sim_mc,
+           nsim = 1000, true.para = true.para, link = "logit",
+           alts = FALSE, bootstrap = FALSE, mc.cores = 6)
+)
