6e7d01d2f7923f6f6a4e95c705fa8954e8b1e3c1
XiaodanLyu <wuhandaxue2013@gmail.com>
2020-04-08 11:40:30 -0500

    Update simulation.R

--- simulation/simulation.R
+++ simulation/simulation.R
@@ -10,21 +10,20 @@ source("simulation/utility-functions.R")
 true.para <- list(beta = c(-13, 2), sig2lu = 0.22, sig2le = 1.23,
                   alpha = c(-20, 5), sig2lb = 0.52)
 
-# set.seed(2020)
 # system.time(
-#   sim_mc(nsim = 3, true.para, rho = 0.9, link = "logit",
-#          alts = FALSE, bootstrap = TRUE)
+#   sim_mc(nsim = 1000, true.para = true.para, rho = 0.9, link = "logit",
+#          alts = FALSE, bootstrap = TRUE, seed = 2015)
 # )
 
-library(parallel)
-rhos <- c(-0.9, -0.6, -0.3, 0, 0.3, 0.6)
+# library(parallel)
+# rhos <- c(-0.9, -0.6, -0.3, 0, 0.3, 0.6)
+# system.time(
+#   mclapply(rhos, sim_mc, nsim = 1000, true.para = true.para, 
+#            link = "logit", alts = FALSE, bootstrap = FALSE,
+#            seed = 2020, mc.cores = 6)
+# )
+
 system.time(
-  mclapply(rhos, sim_mc, nsim = 1000, true.para = true.para, 
-           link = "logit", alts = FALSE, bootstrap = FALSE,
-           seed = 2020, mc.cores = 6)
+  sim_mc(nsim = 1000, true.para = true.para, rho = 0.9, link = "logit",
+         alts = TRUE, bootstrap = FALSE, seed = 2020)
 )
-
-# system.time(
-#   sim_mc(nsim = 1000, true.para, rho = 0.9, link = "logit",
-#          alts = TRUE, bootstrap = FALSE, seed = 2020)
-# )
